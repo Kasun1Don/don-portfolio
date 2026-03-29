66ce0cf8b49d8e62e0283d4c670d4374e360997c
Kasun1Don <144102327+Kasun1Don@users.noreply.github.com>
2026-03-29 23:32:51 +1000

    feat: currently building card

---
+++ src/app/_components/cards/working-on-card.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ChartNoAxesGantt } from "lucide-react";
+
+export function WorkingOnCard() {
+  return (
+    <>
+      <CardHeader className="flex flex-row items-center justify-between pb-4">
+        <CardTitle className="font-medium">Working On</CardTitle>
+        <ChartNoAxesGantt className="text-muted-foreground h-4 w-4" />
+      </CardHeader>
+
+      <CardContent className="flex flex-col items-start gap-2">
+        <Image
+          src="/neopoker.png"
+          alt="NeoPoker"
+          width={100}
+          height={100}
+          className="rounded-xl object-cover"
+        />
+        <p className="text-muted-foreground text-sm">
+          Inspired by Moltbook, building for agents (AX)
+        </p>
+      </CardContent>
+    </>
+  );
+}
