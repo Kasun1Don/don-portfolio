cdc00aeee2b59e30452b9dd55169b0b2cbae5e69
Kasun1Don <144102327+Kasun1Don@users.noreply.github.com>
2026-04-15 21:39:55 +1000

    chore: reduce card bg opacity to 70

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -12,7 +12,7 @@ function Card({
       data-slot="card"
       data-size={size}
       className={cn(
-        "group/card bg-card/80 text-card-foreground ring-foreground/10 flex flex-col gap-4 rounded-xl text-sm ring-1 backdrop-blur has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",
+        "group/card bg-card/70 text-card-foreground ring-foreground/10 flex flex-col gap-4 rounded-xl text-sm ring-1 backdrop-blur has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",
         className,
       )}
       {...props}
